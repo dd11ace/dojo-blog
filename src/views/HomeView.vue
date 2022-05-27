349b6ea7f0b90fa1f71fad2a2f57a94ee83d56a8
Gl <Dimadd11ace@yandex.ru>
2022-05-27 11:46:41 +0500

    Using Refs for Reactivity

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,8 @@
     home
     <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">click me</button>
+    <button @click="age++">add 1 to age</button>
+    <input type="text" v-model="name" />
   </div>
 </template>
 
@@ -11,23 +13,17 @@ import { ref } from '@vue/reactivity';
 export default {
   name: 'HomeView',
   setup() {
-    console.log(this);
+    // const p = ref(null);
 
-    const p = ref(null);
-    console.log(p, p.value);
-
-    console.log('setup');
-
-    let name = 'mario';
-    let age = 30;
+    const name = ref('mario');
+    const age = ref(30);
 
     const handleClick = () => {
-      console.log(p, p.value);
-      p.value.classList.add('test');
-      p.value.textContent = 'hello, ninjas';
+      name.value = 'luigi';
+      age.value = 35;
     };
 
-    return { name, age, handleClick, p };
+    return { name, age, handleClick };
   },
 };
 </script>
