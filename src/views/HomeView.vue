46729f2eb7c9bc3de6ed0ca6b0245d50aa33bbf3
Gl <Dimadd11ace@yandex.ru>
2022-05-27 11:58:02 +0500

    Refs vs Reactive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,36 @@
 <template>
   <div class="home">
-    home
-    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
-    <button @click="handleClick">click me</button>
-    <button @click="age++">add 1 to age</button>
-    <input type="text" v-model="name" />
+    <h1>Home</h1>
+    <h2>Refs</h2>
+    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
+    <button @click="updateNinjaOne">Update ninja one</button>
+    <h2>Reacitive</h2>
+    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }} - {{ nameTwo }}</p>
+    <button @click="updateNinjaTWO">Update ninja two</button>
   </div>
 </template>
 
 <script>
-import { ref } from '@vue/reactivity';
+import { reactive, ref } from '@vue/reactivity';
+
 export default {
   name: 'HomeView',
   setup() {
-    // const p = ref(null);
+    const ninjaOne = ref({ name: 'mario', age: 30 });
+    const ninjaTwo = reactive({ name: 'luigi', age: 35 });
 
-    const name = ref('mario');
-    const age = ref(30);
+    const nameOne = ref('mario');
+    const nameTwo = reactive('luigi'); // can't use primetive values
 
-    const handleClick = () => {
-      name.value = 'luigi';
-      age.value = 35;
+    const updateNinjaOne = () => {
+      ninjaOne.value.age = 40;
     };
 
-    return { name, age, handleClick };
+    const updateNinjaTWO = () => {
+      ninjaTwo.age = 45;
+    };
+
+    return { ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTWO, nameTwo };
   },
 };
 </script>
