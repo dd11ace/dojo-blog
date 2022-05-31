7cd5d87bf30c080a3feeb5479de7efb9faed47d8
Gl <Dimadd11ace@yandex.ru>
2022-05-31 14:20:38 +0500

    Using useRoute

---
+++ src/views/Tag.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="tag">Tag page</div>
+</template>
+
+<script>
+// challenge
+//   - create a tag route, which the tag as a route paramater (/tags/:tag)
+//   - is the Tag comp, make a request for all the posts
+//   - use a computed property to return an array of posts which have the current tag
+//   - use the useRoute function to access the tag route parameter
+//   - use the PostList component to output the required posts
+//   - show the spinner until the data is loaded, and error if there is one
+import Spinner from '../components/Spinner.vue';
+import getPosts from '../composables/getPosts';
+
+export default {};
+</script>
+
+<style></style>
