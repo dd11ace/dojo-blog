e5fd6f48e1f80131502e26ced806eee342caa037
Gl <Dimadd11ace@yandex.ru>
2022-05-31 14:32:37 +0500

    CHALLENGE - Creating a Tag Route

--- src/views/Tag.vue
+++ src/views/Tag.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="tag">Tag page</div>
+  <div class="tag">
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="postsWithTag" />
+    </div>
+    <div v-else>
+      <Spinner />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -11,9 +19,26 @@
 //   - use the PostList component to output the required posts
 //   - show the spinner until the data is loaded, and error if there is one
 import Spinner from '../components/Spinner.vue';
+import PostList from '../components/PostLIst.vue';
 import getPosts from '../composables/getPosts';
+import { useRoute } from 'vue-router';
+import { computed } from '@vue/runtime-core';
 
-export default {};
+export default {
+  components: { PostList, Spinner },
+  setup() {
+    const route = useRoute();
+    const { posts, error, load } = getPosts();
+
+    load();
+
+    const postsWithTag = computed(() => {
+      return posts.value.filter(p => p.tags.includes(route.params.tag));
+    });
+
+    return { error, posts, postsWithTag };
+  },
+};
 </script>
 
 <style></style>
