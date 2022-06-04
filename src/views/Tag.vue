aaff45176c37ab00c4e6ab44c5c99708b2403b72
Gl <Dimadd11ace@yandex.ru>
2022-06-04 19:16:56 +0500

    Creating a Tag Cloud

--- src/views/Tag.vue
+++ src/views/Tag.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="tag">
     <div v-if="error">{{ error }}</div>
-    <div v-if="posts.length">
+    <div v-if="posts.length" class="layout">
       <PostList :posts="postsWithTag" />
+      <TagCloud :posts="posts" />
     </div>
     <div v-else>
       <Spinner />
@@ -11,21 +12,15 @@
 </template>
 
 <script>
-// challenge
-//   - create a tag route, which the tag as a route paramater (/tags/:tag)
-//   - is the Tag comp, make a request for all the posts
-//   - use a computed property to return an array of posts which have the current tag
-//   - use the useRoute function to access the tag route parameter
-//   - use the PostList component to output the required posts
-//   - show the spinner until the data is loaded, and error if there is one
 import Spinner from '../components/Spinner.vue';
 import PostList from '../components/PostLIst.vue';
+import TagCloud from '../components/TagCloud.vue';
 import getPosts from '../composables/getPosts';
 import { useRoute } from 'vue-router';
 import { computed } from '@vue/runtime-core';
 
 export default {
-  components: { PostList, Spinner },
+  components: { PostList, Spinner, TagCloud },
   setup() {
     const route = useRoute();
     const { posts, error, load } = getPosts();
@@ -41,4 +36,10 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.tag {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 10px;
+}
+</style>
