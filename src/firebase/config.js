a689dc3962f57a10af094865ced89a5fe527bf5f
Gl <Dimadd11ace@yandex.ru>
2022-06-04 20:44:40 +0500

    Fetching a Collection of Documents

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,5 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBs6ECCMgtqyLA2LB2zYcQg-7Jjh4toJNg',
