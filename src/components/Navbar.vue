ab887b15c453c6508ca67c1217a9bd3184c6f83e
Gl <Dimadd11ace@yandex.ru>
2022-06-05 16:05:58 +0500

    Installed Sass(scss) && Navbar BEM classes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,37 +1,38 @@
 <template>
-  <header>
-    <h1>The Dojo Blog</h1>
-    <nav>
-      <router-link :to="{ name: 'home' }">Home</router-link>
-      <router-link :to="{ name: 'Create' }">Create Post</router-link>
+  <header class="navbar-header">
+    <h1 class="navbar-header__title">The Dojo Blog</h1>
+    <nav class="navbar-header__list">
+      <router-link :to="{ name: 'home' }" class="navbar-header__list-item"
+        >Home</router-link
+      >
+      <router-link :to="{ name: 'Create' }" class="navbar-header__list-item"
+        >Create Post</router-link
+      >
     </nav>
   </header>
 </template>
 
-<script>
-export default {};
-</script>
-
-<style>
-header {
+<style lang="scss">
+.navbar-header {
   display: flex;
   justify-content: space-between;
   align-items: center;
   max-width: 1200px;
   margin: 0 auto;
   padding: 10px;
-}
-header h1 {
-  color: #dfdfdf;
-  font-size: 48px;
-}
-header a {
-  color: #bbb;
-  text-decoration: none;
-  margin-left: 20px;
-}
-header a.router-link-active {
-  color: #444;
-  font-weight: bold;
+
+  &__title {
+    color: #dfdfdf;
+    font-size: 48px;
+  }
+  &__list-item {
+    color: #bbb;
+    text-decoration: none;
+    margin-left: 20px;
+  }
+  &__list-item.router-link-active {
+    color: #444;
+    font-weight: bold;
+  }
 }
 </style>
